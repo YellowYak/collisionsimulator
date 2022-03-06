931c0f771a5fd002d47a9ca503621705316205f5
Scott Mitchell <scott.k.mitchell@gmail.com>
2022-03-05 20:06:07 -0800

    Added support for object collision. [WIP]

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 import { Canvas } from './modules/canvas.js';
 import { Particle } from './modules/particle.js';
 
-let myCanvas = new Canvas('myCanvas', document.body, 480, 320);
+var colorMap = ['rgb(120,28,129)', 'rgb(67,40,151)', 'rgb(65,111,184)', 'rgb(81,156,184)', 'rgb(112,180,132)', 'rgb(153,189,92)', 'rgb(195,186,69)', 'rgb(224,162,57)', 'rgb(230,107,45)', 'rgb(217,33,32)'];
+
+let myCanvas = new Canvas('myCanvas', document.body, 800, 600);
 myCanvas.create();
 
 const particles = [];
-particles.push(new Particle('Big Red', 130, 130, 40, 40, 3.5, 205, '#ff0000'));
-particles.push(new Particle('Balpha', 230, 130, 10, 20, 12, 275, '#00ff00'));
-particles.push(new Particle('Blue', 350, 250, 45, 20, 2.5, 222, '#0000ff'));
+particles.push(new Particle({ name: 'Scott', x: 333, y: 200, radius: 100, mass: 8, speed: 2, direction: 155, color: colorMap[7] }));
+particles.push(new Particle({ name: 'Big Boy', x: 130, y: 130, radius: 40, mass: 4, speed: 1, direction: 13, color: colorMap[3] }));
+particles.push(new Particle({ name: 'Big Boy', x: 230, y: 312, radius: 40, mass: 4, speed: 1, direction: 69, color: colorMap[3] }));
+particles.push(new Particle({ name: 'Big Boy', x: 30, y: 30, radius: 40, mass: 4, speed: 1, direction: 113, color: colorMap[3] }));
+particles.push(new Particle({ name: 'Big Boy', x: 111, y: 120, radius: 40, mass: 4, speed: 1, direction: 321, color: colorMap[3] }));
+particles.push(new Particle({ name: 'Big Boy', x: 333, y: 100, radius: 40, mass: 4, speed: 1, direction: 11, color: colorMap[3] }));
+particles.push(new Particle({ name: 'Big Boy', x: 233, y: 230, radius: 40, mass: 4, speed: 1, direction: 188, color: colorMap[3] }));
 
 
-setInterval(updateWorld, 20);
+setInterval(updateWorld, 1);
 
 function updateWorld() {
     myCanvas.clear();
 
-    particles.forEach(p => {
-        p.updatePosition(myCanvas);
-        p.draw(myCanvas.ctx)
-    });
+    for (let i = 0; i < particles.length; i++) {
+        const currentParticle = particles[i];
+        currentParticle.updatePosition(myCanvas);
+        currentParticle.draw(myCanvas.ctx);
+
+        for (let j = i + 1; j < particles.length; j++) {
+            const checkingParticle = particles[j];
+            if (currentParticle.collidedWith(checkingParticle)) {
+                console.log(`${currentParticle.name} collided with ${checkingParticle.name}`);
+                currentParticle.collision(checkingParticle);
+            }
+        }
+    };
+
+
 }
--- modules/particle.js
+++ modules/particle.js
@@ -1,35 +1,118 @@
 class Particle {
-    constructor(name, x, y, width, height, speed, direction, color) {
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.speed = speed;
-        this.direction = direction;
-        this.color = color;
+    constructor(params) {
+        this.name = params.name;
+        this.x = params.x;
+        this.y = params.y;
+        this.radius = params.radius;
+        this.mass = params.mass;
+        this.speed = params.speed;
+        this.direction = params.direction;
+        this.color = params.color;
     }
 
     draw(ctx) {
-        console.log(`Drawing ${this.name} at (${this.x}, ${this.y})`)
+        //console.log(`Drawing ${this.name} at (${this.x}, ${this.y})`)
 
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillRect(this.x, this.y, this.radius, this.radius);
     }
 
-    degrees_to_radians(degrees)
+    degreesToRadians(degrees)
     {
         return degrees * (Math.PI / 180);
     }
 
+    radiansToDegrees(radians)
+    {
+        return 180 * radians / Math.PI;
+    }
+
+    collidedWith(otherParticle) {
+        const otherLY = (otherParticle.y + otherParticle.radius);
+        const thisLY = this.y + this.radius;
+        const overlappingY = otherLY > this.y && otherParticle.y <= thisLY;
+
+        const otherRX = (otherParticle.x + otherParticle.radius);
+        const thisRX = this.x + this.radius;
+        const overlappingX = otherRX > this.x && otherParticle.x <= thisRX;
+
+        return overlappingX && overlappingY;
+    }
+
+    collision(otherParticle) {
+        let m1 = this.mass;
+        let v1ix = Math.cos(this.degreesToRadians(this.direction)) * this.speed;
+        let v1iy = Math.sin(this.degreesToRadians(this.direction)) * this.speed;
+        let v1i = Math.sqrt(v1ix * v1ix + v1iy * v1iy);
+
+        let m2 = otherParticle.mass;
+        let v2ix = Math.cos(this.degreesToRadians(otherParticle.direction)) * otherParticle.speed;
+        let v2iy = Math.sin(this.degreesToRadians(otherParticle.direction)) * otherParticle.speed;
+        let v2i = Math.sqrt(v2ix * v2ix + v2iy * v2iy);
+
+        // Determine collision angle
+        const dx = v2ix - v1ix;
+        const dy = v2iy - v2iy;
+        let phi = Math.PI / 2;
+        if (dx !== 0) {
+            phi = Math.atan(dy / dx);
+        }
+
+        const ang1 = this.findAngle(v1ix, v1iy);
+        const ang2 = this.findAngle(v2ix, v2iy);
+
+        let v1xr = v1i * Math.cos(ang1 - phi);
+        let v1yr = v1i * Math.sin(ang1 - phi);
+        let v2xr = v2i * Math.cos(ang2 - phi);
+        let v2yr = v2i * Math.sin(ang2 - phi);
+
+        let v1fxr = (m1 - m2) / (m1 + m2) * v1xr + (2 * m2) / (m1 + m2) * v2xr;
+        let v2fxr = (m2 - m1) / (m2 + m1) * v2xr + (2 * m1) / (m2 + m1) * v1xr;
+        let v1fyr = v1yr;
+        let v2fyr = v2yr;
+
+        let v1fx = Math.cos(phi) * v1fxr + Math.cos(phi + Math.PI / 2) * v1fyr;
+        let v1fy = Math.sin(phi) * v1fxr + Math.sin(phi + Math.PI / 2) * v1fyr;
+        let v1f = Math.sqrt(v1fx * v1fx + v1fy * v1fy);
+        
+        let v2fx = Math.cos(phi) * v2fxr + Math.cos(phi + Math.PI / 2) * v2fyr;
+        let v2fy = Math.sin(phi) * v2fxr + Math.sin(phi + Math.PI / 2) * v2fyr;
+        let v2f = Math.sqrt(v2fx * v2fx + v2fy * v2fy);
+        
+        let ang1f = this.radiansToDegrees(this.findAngle(v1fx, v1fy));
+        let ang2f = this.radiansToDegrees(this.findAngle(v2fx, v2fy));
+        
+        this.speed = v1f;
+        this.direction = ang1f;
+
+        otherParticle.speed = v2f;
+        otherParticle.direction = ang2f;
+    }
+
+    findAngle(x, y) {
+        if (x < 0) {
+            return Math.PI + Math.atan(y / x);
+        } else if (x > 0 && y >= 0) {
+            return Math.atan(y / x);
+        } else if (x > 0 && y < 0) {
+            return 2 * Math.PI + Math.atan(y / x);
+        } else if (x === 0 && y === 0) {
+            return 0;
+        } else if (x === 0 && y >= 0) {
+            return Math.PI / 2;
+        } else {
+            return 3 * Math.PI / 2;
+        }
+    }
+
     updatePosition(canvas) {
-        console.log(`Current position for ${this.name} is (${this.x}, ${this.y}) with direction ${this.direction}`);
+        //console.log(`Current position for ${this.name} is (${this.x}, ${this.y}) with direction ${this.direction}`);
 
-        let newX = this.x + Math.cos(this.degrees_to_radians(this.direction)) * this.speed;
-        let newY = this.y - Math.sin(this.degrees_to_radians(this.direction)) * this.speed;
+        let newX = this.x + Math.cos(this.degreesToRadians(this.direction)) * this.speed;
+        let newY = this.y - Math.sin(this.degreesToRadians(this.direction)) * this.speed;
 
-        const newRX = newX + this.width;
-        const newLY = newY + this.height;
+        const newRX = newX + this.radius;
+        const newLY = newY + this.radius;
 
         if (newX <= 0) {
             newX = 0;
@@ -42,18 +125,18 @@ class Particle {
             newY = 0;
             if (this.direction <= 90) {
                 this.direction = 360 - this.direction;
-            } else {
+            } else { 
                 this.direction = 270 - (this.direction - 90);
             }
         } else if (newRX >= canvas.width) {
-            newX = canvas.width - this.width;
+            newX = canvas.width - this.radius;
             if (this.direction <= 90) {
                 this.direction = 180 - this.direction;
             } else {
                 this.direction = 360 - this.direction + 180;
             }
         } else if (newLY >= canvas.height) {
-            newY = canvas.height - this.height;
+            newY = canvas.height - this.radius;
             if (this.direction <= 270) {
                 this.direction = (270 - this.direction) + 90;
             } else {
@@ -66,7 +149,7 @@ class Particle {
         this.x = newX;
         this.y = newY;
 
-        console.log(`New position for ${this.name} is (${this.x}, ${this.y}) with direction ${this.direction}`);
+        //console.log(`New position for ${this.name} is (${this.x}, ${this.y}) with direction ${this.direction}`);
 
     }
 }
