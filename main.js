9471e3c7165fd3bf9a31a19652c5d9566d9f5f91
Scott Mitchell <scott.k.mitchell@gmail.com>
2022-03-06 16:47:27 -0800

    Added StatusBoard class to show <table> of particles.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Canvas } from './modules/canvas.js';
+import { StatusBoard } from './modules/statusBoard.js';
 import { Particle } from './modules/particle.js';
 
 var colorMap = ['rgb(120,28,129)', 'rgb(67,40,151)', 'rgb(65,111,184)', 'rgb(81,156,184)', 'rgb(112,180,132)', 'rgb(153,189,92)', 'rgb(195,186,69)', 'rgb(224,162,57)', 'rgb(230,107,45)', 'rgb(217,33,32)'];
@@ -6,10 +7,13 @@ var colorMap = ['rgb(120,28,129)', 'rgb(67,40,151)', 'rgb(65,111,184)', 'rgb(81,
 let myCanvas = new Canvas('myCanvas', document.body, 800, 600);
 myCanvas.create();
 
+let myStatusBoard = new StatusBoard(document.body);
+myStatusBoard.create();
+
 const particles = [];
-const PARTICLE_COUNT = 50;
+const PARTICLE_COUNT = 25;
 for (let i = 0; i < PARTICLE_COUNT; i++) {
-    const mass = getRandomInt(9);
+    const mass = getRandomInt(3) + 1;
     particles.push(
         new Particle({ 
             name: `Particle ${i+1}`,
@@ -17,17 +21,22 @@ for (let i = 0; i < PARTICLE_COUNT; i++) {
             y: getRandomInt(590),
             radius: 10,
             mass: mass, 
-            speed: getRandomInt(10) + 1,
+            speed: getRandomInt(5) + 1,
             direction: getRandomInt(360),
             color: colorMap[mass]
         })
     );
 }
 
-setInterval(updateWorld, 1);
+let iteration = 0;
+setInterval(updateWorld, 25);
 
 function updateWorld() {
     myCanvas.clear();
+
+    if (iteration % 10 === 0) {
+        myStatusBoard.update(particles);
+    }
 
     for (let i = 0; i < particles.length; i++) {
         const currentParticle = particles[i];
@@ -42,6 +51,8 @@ function updateWorld() {
             }
         }
     };
+
+    iteration++;
 }
 
 function getRandomInt(max) {
---
+++ modules/statusBoard.js
@@ -0,0 +1,77 @@
+class StatusBoard {
+    constructor(parent) {
+        this.parent = parent;
+
+        this.table = null;
+        this.tbody = null;
+    }
+
+    create() {
+        if (this.table === null) {
+            // Create table
+            this.table = document.createElement('table');
+            this.parent.appendChild(this.table);
+
+            const thead = document.createElement('thead');
+            this.table.appendChild(thead);
+
+            const theadTr = document.createElement('tr');
+            thead.appendChild(theadTr);
+
+            let th = document.createElement('th');
+            th.innerText = 'Particle';
+            theadTr.appendChild(th);
+
+            th = document.createElement('th');
+            th.innerText = 'Mass';
+            theadTr.appendChild(th);
+
+            th = document.createElement('th');
+            th.innerText = 'Position';
+            theadTr.appendChild(th);
+
+            th = document.createElement('th');
+            th.innerText = 'Speed';
+            theadTr.appendChild(th);
+
+            th = document.createElement('th');
+            th.innerText = 'Direction';
+            theadTr.appendChild(th);
+
+            this.tbody = document.createElement('tbody');
+            this.table.appendChild(this.tbody);
+        }
+    }
+    
+    update(particles) {
+        this.tbody.replaceChildren();
+
+        particles.forEach(p => {
+            const tr = document.createElement('tr');
+            this.tbody.appendChild(tr);
+
+            let td = document.createElement('td');
+            td.innerText = p.name;
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerText = p.mass;
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerText = `(${p.x.toFixed(0)}, ${p.y.toFixed(0)})`;
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerText = p.speed.toFixed(1);
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerHTML = `${p.direction.toFixed(0)}&deg`;
+            tr.appendChild(td);
+        });
+    }
+}
+
+
+export { StatusBoard };
